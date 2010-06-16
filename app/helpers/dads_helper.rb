3fd00a89680fe790a49bdb0db5ecd2204bf8257f
Michael Maloney <maloney.mc@gmail.com>
2010-06-16 14:34:05 -0400

    stuck in Facebook shit. probably does not work.

--- app/helpers/dads_helper.rb
+++ app/helpers/dads_helper.rb
@@ -1,2 +1,9 @@
 module DadsHelper
+  def facebook_share(link)
+    %{<a href="http://www.facebook.com/sharer.php?u=#{link}&t='Who is your daddy and what does he do?'">Facebook!</a>}
+  end
+  
+  def twitter_share(link)
+    %{<a href="http://www.twitter.com/home?status=Who+is+you+daddy+and+what+does+he+do?+<%= Dad.make_bitly_from('#{link}') %>">Twitter!</a>}
+  end
 end
