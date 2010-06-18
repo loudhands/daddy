78240aeae3f215baca5b4a3646be941d1839bea4
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-06-18 18:30:06 -0400

    twitter formatting stuff

--- app/models/dad.rb
+++ app/models/dad.rb
@@ -1,7 +1,12 @@
 class Dad < ActiveRecord::Base
   require 'bitly'
+  include ActionView::Helpers::TextHelper
   
   validates_presence_of :name
+  
+  validates_length_of :description, :maximum => 140
+  
+  validates_length_of :name, :maximum => 50
   
   validates_less_reverse_captcha
   
@@ -14,5 +19,22 @@ class Dad < ActiveRecord::Base
   def self.make_bitly_from(link)
     bitly = Bitly.new("mcmaloney", "R_e671a79e4b4482795dc870aeb4194df6")
     bitly.shorten(link).short_url
+  end
+  
+  def to_twitter(link)
+    "My+Dad+is+#{self.name}+and+he+#{truncate(self.twitter_description, :length => 50)}+#{Dad.make_bitly_from(link)}"
+  end
+  
+  def twitter_description
+    desc = ""
+    if self.description.size > 50
+      self.description.split(" ").each do |word|
+        desc += word
+        desc += "+"
+      end
+      desc.chop
+    else
+      self.description
+    end
   end
 end
