3845715079de7455f5c3f4aab259973907c456c7
robertjwhitney <robertj.whitney@gmail.com>
2010-06-19 18:52:05 -0400

    styling dad show

--- app/helpers/dads_helper.rb
+++ app/helpers/dads_helper.rb
@@ -1,13 +1,13 @@
 module DadsHelper
   def facebook_share(link)
-    %{<a href="http://www.facebook.com/sharer.php?u=#{link}&t='Who is your daddy and what does he do?'">Facebook!</a>}
+    %{<a href="http://www.facebook.com/sharer.php?u=#{link}&t='Who is your daddy and what does he do?'" class="fb">Facebook</a>}
   end
   
   def twitter_share(dad, link)
-    %{<a href="http://www.twitter.com/home?status=#{dad.to_twitter(link)}">Twitter!</a>}
+    %{<a href="http://www.twitter.com/home?status=#{dad.to_twitter(link)}" class="tw">Twitter</a>}
   end
   
   def email_share(link)
-    %{<a href="mailto:?subject=Dads!%20&body=Check%20out%20my%20dad:%20#{link}">Email This</a>}
+    %{<a href="mailto:?subject=Dads!%20&body=Check%20out%20my%20dad:%20#{link}" class="em">Email This</a>}
   end
 end
