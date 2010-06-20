b8792b83c113b71cbb782ccd9c60abedc073c843
robertjwhitney <robertj.whitney@gmail.com>
2010-06-20 11:05:55 -0400

    making dad's descending

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,6 @@
 class PublicController < ApplicationController
   def home
-    @dads = Dad.paginate(:all, :page => params[:page], :per_page => 25)
+    @dads = Dad.paginate(:all, :page => params[:page], :per_page => 25, :order=> 'created_at DESC')
   end
 
 end
